b4ae6d755b58a0dd4e11033248d5f252aa1d7634
Jacky11223 <103395254+Jacky11223@users.noreply.github.com>
2022-05-04 19:09:56 +0800

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    var x;
+   
+    x = document.getElementById("myText").value;
+   
+   
+    if (x=="student") {
+    window.location.replace("https://jacky11223.github.io/EX54/COURSE.html")
+    } else if (x=="parent"){
+    window. location.replace("https://jacky11223.github.io/EX54/index.html")
+    
+    }
+    else if (x!="student"){
+   alert("Please ")
+  }
+   else if (x!="parent"){
+   alert("Please !")
+    
+    }
+   } 
